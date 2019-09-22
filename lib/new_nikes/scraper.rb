435a9ab205ce8bab8273974d65d01aea2ad25cfc
Maverick Adolfo <merkonical@gmail.com>
2019-09-22 18:40:31 +0000

    Scraper made

--- lib/new_nikes/scraper.rb
+++ lib/new_nikes/scraper.rb
@@ -1,3 +1,25 @@
 class Scraper 
-  
+    attr_accessor :name, :price
+
+  def self.today
+    self.scrape_shoes
+  end
+
+  def self.scrape_shoes
+    shoes = []
+
+    shoes << self.scrape_nike
+    
+    shoes
+  end
+
+  def self.scrape_nike
+    doc = Nokogiri::HTML(open("https://www.nike.com/w/new-mens-shoes-3n82yznik1zy7ok?sort=newest" 'User-Agent' => 'firefox'))
+
+    shoe = self.new
+    shoe.name = doc.search("div.product-card__title").text.strip
+    shoe.price = doc.search("div.product-card__price").text.strip
+    
+    shoe
+  end
 end 
