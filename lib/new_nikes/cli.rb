0d5a37743a8de447ac6d3f7bbd24c075339fe86e
merkonical <merkonical@gmail.com>
2019-12-12 23:05:47 -0800

    Fix Cli to work

--- lib/new_nikes/cli.rb
+++ lib/new_nikes/cli.rb
@@ -1,49 +1,48 @@
 class NewNikes::CLI 
  
  def list_shoes
-    NewNikes::Scraper.scrape 
-    NewNikes::Scraper.scrape_shoe
-    NewNikes::Scraper.display_shoe
+  NewNikes::Scraper.scrape 
+  NewNikes::Scraper.scrape_shoe
+  NewNikes::Scraper.display_shoe
  end
   
-  def list_price(input)
-    NewNikes::Scraper.scrape
-    NewNikes::Scraper.scrape_shoe
-    NewNikes::Scraper.display_price(input)
-  end
+ def list_price(input)
+  NewNikes::Scraper.scrape
+  NewNikes::Scraper.scrape_shoe
+  NewNikes::Scraper.display_price(input)
+ end
   
-  def call
-    welcome
-    puts"   "
-    options 
+ def call
+  welcome
+  puts"   "
+  options 
     
-  end
+ end
   
-  def welcome
-    puts "This is the new Nike shoes for men section"
-  end 
-  def options
-    input = "nil"
-    puts "Please enter your input below this line with name or price"
-    puts "-----------------------------------------"
-      puts "   "
-      input = gets.strip.downcase
-      case input
-      when "name"
-        puts "-------------------------"
-        puts "   "
-        list_shoes
-        puts "-------------------------"
-          options 
-          puts "  "
-      when "price"
-        puts "showing nike price"
-        puts "-------------------------"
-          list_price(input)
-        options
-        puts "-------------------------"
-      
-      end 
-    end
-  end 
+ def welcome
+  puts "This is the new Nike shoes for men section"
+ end 
+  
+ def options
+  input = "nil"
+  puts "Please enter your input below this line with name or price"
+  puts "-----------------------------------------"
+  puts "   "
+  input = gets.strip.downcase
+  case input
+   when "name"
+    puts "-------------------------"
+    puts "   "
+    list_shoes
+    puts "-------------------------"
+    options 
+    puts "  "
+   when "price"
+    puts "showing nike price"
+    puts "-------------------------"
+    list_price(input)
+    options
+    puts "-------------------------"
+   end    
+  end
 end 
