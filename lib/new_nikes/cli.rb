8c388106187bac90df66de9429406e6682a8d386
merkonical <merkonical@gmail.com>
2019-12-14 00:05:51 -0800

    modified cli,scraper,and shoe

--- lib/new_nikes/cli.rb
+++ lib/new_nikes/cli.rb
@@ -1,21 +1,22 @@
 class NewNikes::CLI 
  
- def list_shoes
-  NewNikes::Scraper.scrape 
-  NewNikes::Scraper.scrape_shoe
-  NewNikes::Scraper.display_shoe
- end
+  def list_shoes
+    NewNikes::Shoe.all.each.with_index(1) do |shoe, i|
+      puts "   #{i}.   #{shoe.name}"
+    end
+  end
   
- def list_price
-  NewNikes::Scraper.scrape
-  NewNikes::Scraper.scrape_shoe
-  NewNikes::Scraper.display_price(input)
- end
+  def display_price(input)
+    index = input.to_i - 1
+    puts "#{NewNikes::Shoe.all[index].price}"
+  end
   
  def call
   welcome
-  list_price
   puts"   "
+  NewNikes::Scraper.scrape 
+  NewNikes::Scraper.scrape_shoe
+  list_shoes
   options 
  end
   
@@ -25,24 +26,38 @@ class NewNikes::CLI
   
  def options
   input = "nil"
-  puts "Please enter your input below this line with name or price"
+  puts "Please input number of shoe for more info"
+  puts "type list to see another shoe"
   puts "-----------------------------------------"
-  puts "   "
   input = gets.strip.downcase
-  case input
-   when "name"
-    puts "-------------------------"
-    puts "   "
-    list_shoes
-    puts "-------------------------"
-    options 
-    puts "  "
-   when "price"
-    puts "showing nike price"
-    puts "-------------------------"
-    list_price(input)
-    options
-    puts "-------------------------"
-   end    
+    if input.to_i > 0 && input.to_i <= NewNikes::Shoe.all.length  #input is a number 1 to last number of shoe displayed
+      display_price(input)
+      puts "would you like to look at another shoe?"
+      input = gets.strip.downcase
+      if input == "y"
+      options
+      end
+    elsif input == "list"
+      list_shoes
+      options
+    else                    #wrong input
+      puts "invalid input, please choose another number"
+      options
+    end 
+    # case input
+    #   when "name"
+    #   puts "-------------------------"
+    #   puts "   "
+    #   list_shoes
+    #   puts "-------------------------"
+    #   options 
+    #   puts "  "
+    #   when "price"
+    #   puts "showing nike price"
+    #   puts "-------------------------"
+    #   list_price
+    #   options
+    #   puts "-------------------------"
+    # end    
   end
 end 
--- lib/new_nikes/scraper.rb
+++ lib/new_nikes/scraper.rb
@@ -1,5 +1,7 @@
 class NewNikes::Scraper 
 
+
+ 
   def self.scrape
     @doc = Nokogiri::HTML(open("https://www.nike.com/w/new-mens-shoes-3n82yznik1zy7ok?sort=newest", 'User-Agent' => 'firefox'))
     
@@ -9,20 +11,9 @@ class NewNikes::Scraper
     @doc.css("div.product-card__body").each do |nike|
       name = nike.css("div.product-card__title").text.strip
       price = nike.css("div.product-card__price").text.strip
-      shoes = self.new
-      @all << shoes
-      binding.pry
-    end 
+      shoes = NewNikes::Shoe.new(name, price)
+    end
   end 
   
-  def self.display_shoe
-    @all.each.with_index(1) do |shoe, i|
-      puts "   #{i}.   #{shoe.name}"
-    end
-  end
-  
-  def self.display_price(input)
-    index = input.to_i - 1
-    puts "#{@all[index].price}"
-  end
+
 end 
