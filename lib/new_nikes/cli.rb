4a21e6016c931338939a1d56cd4e16e2970e54f5
merkonical <merkonical@gmail.com>
2019-12-15 21:52:22 -0800

    made changes to CLI

--- lib/new_nikes/cli.rb
+++ lib/new_nikes/cli.rb
@@ -21,25 +21,31 @@ class NewNikes::CLI
  end
   
  def welcome
-  puts "This is the new Nike shoes for men section"
+  puts "Welcome! This is the new Nike shoes for men section"
+  puts "Please choose a number"
  end 
   
  def options
   input = "nil"
-  puts "Please input number of shoe for more info"
-  puts "type list to see another shoe"
+  puts "Please input number of shoe from the list for its price"
+  puts "type list to see the shoes again"
+  puts "or type exit"
   puts "-----------------------------------------"
   input = gets.strip.downcase
     if input.to_i > 0 && input.to_i <= NewNikes::Shoe.all.length  #input is a number 1 to last number of shoe displayed
       display_price(input)
-      puts "would you like to look at another shoe?"
+      puts "would you like to look at another shoe? y/n"
       input = gets.strip.downcase
       if input == "y"
       options
+      else input == "n"
+        puts "Thank you! Come again."
       end
     elsif input == "list"
       list_shoes
       options
+    elsif input == "exit"
+      puts "Thank you! Come again."
     else                    #wrong input
       puts "invalid input, please choose another number"
       options
